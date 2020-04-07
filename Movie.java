c1c41fee53b3abe09187b46090082b8c3bbcd46d
Charles Coffin <cdcoffin@email.sc.edu>
2020-04-07 19:18:36 -0400

    added JavaDoc

--- Movie.java
+++ Movie.java
@@ -2,8 +2,18 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
+/**
+ * Stores a Movie object, a subclass of Event
+ */
 public class Movie extends Event {
 	
+	/**
+	 * Default Constructor: generates new movie (full of empty seats)
+	 * @param name - name of movie
+	 * @param venue - venue object
+	 * @param date - event date in LocalDate format
+	 * @param time - event time in LocalTime format
+	 */
 	public Movie(String name, Venue venue, LocalDate date, LocalTime time) {
 		this.name = name;
 		this.venue = venue;
@@ -14,6 +24,14 @@ public class Movie extends Event {
 		this.reviews = new ArrayList<Review>();
 	}
 	
+	/**
+	 * Alternate Constructor: generates existing movie (using seat array) - mainly for database
+	 * @param name - name of movie
+	 * @param venue - venue object
+	 * @param date - event date in LocalDate format
+	 * @param time - event time in LocalTime format
+	 * @param seats - boolean array of available seats
+	 */
 	public Movie(String name, Venue venue, LocalDate date, LocalTime time, boolean[][] seats) {
 		this.name = name;
 		this.venue = venue;
