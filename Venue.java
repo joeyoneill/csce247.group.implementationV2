3aed3c16b96897cc1fde2bca4e6d43a4a9f63cc4
joeyoneill <o39joey@gmail.com>
2020-04-06 22:38:38 -0400

    Venue updated

--- Venue.java
+++ Venue.java
@@ -2,19 +2,28 @@
 public class Venue {
 	
 	private String name;
+	public int rows;
+	public int columns;
+	public boolean[][] seats; //false = not taken
 	
-	public Venue(String name) {
+	public Venue(String name, int rows, int columns) {
 		this.name = name;
+		this.rows = rows;
+		this.columns = columns;
+		seats = new boolean[rows][columns];
 	}
 	
 	// Getters
 	public String getName() {
 		return name;
 	}
-	
-	// Setters
-	public void setName(String name) {
-		this.name = name;
+	public int getRows() {
+		return rows;
 	}
-	
+	public int getColumns() {
+		return columns;
+	}
+	public boolean[][] getSeats() {
+		return seats;
+	}
 }
