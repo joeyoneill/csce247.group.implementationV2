733c4959baa2ebcda08d83d019a87a44c8260f5c
joeyoneill <o39joey@gmail.com>
2020-04-06 22:00:27 -0400

    abstract user

---
+++ User.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public abstract class User {
+	private String name;
+	private UserType usertype;
+	private ArrayList<Ticket> purchasedTickets = new ArrayList<Ticket>();
+	
+	
+}
