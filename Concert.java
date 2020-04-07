1b88a936fe891c6cc1c9242f7b37f33ff18b8757
joeyoneill <o39joey@gmail.com>
2020-04-07 00:10:33 -0400

    Event sub extra constructor

--- Concert.java
+++ Concert.java
@@ -14,6 +14,16 @@ public class Concert extends Event {
 		this.reviews = new ArrayList<Review>();
 	}
 	
+	public Concert(String name, Venue venue, LocalDate date, LocalTime time, boolean[][] seats) {
+		this.name = name;
+		this.venue = venue;
+		this.type = "Concert";		
+		this.date = date;
+		this.time = time;
+		this.seats = seats;
+		this.reviews = new ArrayList<Review>();
+	}
+	
 	@Override
 	public String getType() {
 		return type;
--- Play.java
+++ Play.java
@@ -14,6 +14,16 @@ public class Play extends Event {
 		this.reviews = new ArrayList<Review>();
 	}
 	
+	public Play(String name, Venue venue, LocalDate date, LocalTime time, boolean[][] seats) {
+		this.name = name;
+		this.venue = venue;
+		this.type = "Play";		
+		this.date = date;
+		this.time = time;
+		this.seats = seats;
+		this.reviews = new ArrayList<Review>();
+	}
+	
 	@Override
 	public String getType() {
 		return type;
